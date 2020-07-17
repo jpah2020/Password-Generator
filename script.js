2c8971e03749bd18abc10a3db09c02526eb7c1d4
Rusky <jpah.design@gmail.com>
2020-07-17 16:25:07 -0400

    fix style

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 //generate random password
 function generate() {
 	var complexity = document.getElementById('slider').value;
-	
-		var values = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-+1234567890';
-		
-		var password = '';
 
-		for (var i = 0; i <= complexity; i++) {
+	//possible password values
+	var values = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-+1234567890';
+
+	var password = '';
+
+	for (var i = 0; i <= complexity; i++) {
 		password =
 			password +
 			values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
 	}
 
-	document.getElementById('password').value = password;
+	document.getElementById('display').value = password;
 }
